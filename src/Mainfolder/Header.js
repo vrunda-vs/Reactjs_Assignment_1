81f1fc3ac69467af42dde9095a3728031a5dc490
vrunda-vs <71866329+vrunda-vs@users.noreply.github.com>
2021-01-28 12:44:57 +0530

    Initial commit

---
+++ src/Mainfolder/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+import './Style.css'
+function Header(){
+    return(
+        <div>
+            <h1 className="H1">Header page</h1>
+        </div>
+    )
+};
+export default Header;
